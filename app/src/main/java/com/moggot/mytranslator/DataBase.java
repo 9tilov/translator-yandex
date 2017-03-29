6c1d9b09a008794e420aaed01a05c5ebc1a81ad6
moggot <9.tilov@gmail.com>
2017-03-29 11:48:13 +0300

    добавил класс БД

--- app/src/main/java/com/moggot/mytranslator/DataBase.java
+++ app/src/main/java/com/moggot/mytranslator/DataBase.java
@@ -1,20 +1,14 @@
-package com.moggot.commonalarmclock;
+package com.moggot.mytranslator;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.SparseIntArray;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.moggot.commonalarmclock.alarm.Alarm;
-import com.moggot.commonalarmclock.alarm.AlarmDao;
-import com.moggot.commonalarmclock.alarm.DaoMaster;
-import com.moggot.commonalarmclock.alarm.DaoSession;
+import com.moggot.mytranslator.translator.DaoMaster;
+import com.moggot.mytranslator.translator.DaoSession;
+import com.moggot.mytranslator.translator.Translator;
+import com.moggot.mytranslator.translator.TranslatorDao;
 
-import java.lang.reflect.Type;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by toor on 01.03.17.
@@ -23,60 +17,41 @@ import java.util.Random;
 public class DataBase {
 
     private Context context;
-    private AlarmDao alarmDao;
+    private TranslatorDao translatorDao;
 
     private final String DB_NAME = "alarm_db";
 
     public DataBase(Context context) {
         this.context = context;
-        alarmDao = setupDb();
+        translatorDao = setupDb();
     }
 
-    public void addAlarm(Alarm alarm) {
-        alarmDao.insert(alarm);
+    public void addRecord(Translator record) {
+        translatorDao.insert(record);
     }
 
-    public void editAlarm(Alarm alarm) {
-        alarmDao.update(alarm);
+    public void editRecord(Translator record) {
+        translatorDao.update(record);
     }
 
-    public void deleteAlarm(Alarm alarm) {
-        alarmDao.delete(alarm);
+    public void deleteRecord(Translator record) {
+        translatorDao.delete(record);
     }
 
-    public Alarm getAlarm(long id) {
-        return alarmDao.load(id);
+    public Translator getTranslator(long id) {
+        return translatorDao.load(id);
     }
 
-    public List<Alarm> getAllAlarms() {
-        return alarmDao.queryBuilder().build().list();
+    public List<Translator> getAllRecords() {
+        return translatorDao.queryBuilder().build().list();
     }
 
-    private AlarmDao setupDb() {
+    private TranslatorDao setupDb() {
         DaoMaster.DevOpenHelper masterHelper = new DaoMaster.DevOpenHelper(context, DB_NAME, null); //create database db file if not exist
         SQLiteDatabase db = masterHelper.getWritableDatabase();  //get the created database db file
         DaoMaster master = new DaoMaster(db);//create masterDao
         DaoSession masterSession = master.newSession(); //Creates Session session
-        return masterSession.getAlarmDao();
-    }
-
-    public int getRandomRequestCode() {
-        HashSet<Integer> unique = new HashSet<>();
-        List<Alarm> alarms = getAllAlarms();
-        for (Alarm alarm : alarms) {
-            Type type = new TypeToken<SparseIntArray>() {
-            }.getType();
-            SparseIntArray ids = new Gson().fromJson(alarm.getRequestCodes(), type);
-            for (int i = 0; i < ids.size(); ++i) {
-                unique.add(ids.valueAt(i));
-            }
-        }
-        Random rnd = new Random();
-        int requestCode;
-        do {
-            requestCode = rnd.nextInt(Integer.MAX_VALUE);
-        } while (unique.contains(requestCode));
-        return requestCode;
+        return masterSession.getTranslatorDao();
     }
 
 }
